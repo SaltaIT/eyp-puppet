9246a6310235c156a601c09a7bab710203e628fa
Jordi Prats <jprats@systemadmin.es>
2016-03-02 18:13:29 +0100

    correció fact wget

--- lib/facter/eyp_apache_make.rb
+++
@@ -1,9 +0,0 @@
-make = Facter::Util::Resolution.exec('bash -c \'wget -v 2>&1 | head -n1 | grep -i wget\'').to_s
-
-unless make.nil? or make.empty?
-  Facter.add('eyp_puppet_wget') do
-      setcode do
-        make
-      end
-  end
-end
---
+++ lib/facter/eyp_apache_wget.rb
@@ -0,0 +1,9 @@
+wget = Facter::Util::Resolution.exec('bash -c \'wget -v 2>&1 | head -n1 | grep -i wget\'').to_s
+
+unless wget.nil? or wget.empty?
+  Facter.add('eyp_puppet_wget') do
+      setcode do
+        wget
+      end
+  end
+end
