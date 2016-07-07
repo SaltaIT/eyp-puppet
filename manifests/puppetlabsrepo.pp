06ccb5e010a4777aa64172908834c637cf6f844f
Jordi Prats <jprats@systemadmin.es>
2016-07-07 13:39:50 +0200

    lint

--- manifests/puppetlabsrepo.pp
+++ manifests/puppetlabsrepo.pp
@@ -1,6 +1,6 @@
 class puppet::puppetlabsrepo(
                               $enable_puppetlabs_repo,
-                              $srcdir='/usr/local/src',
+                              $srcdir = '/usr/local/src',
                             ) inherits puppet::params {
 
   Exec {
