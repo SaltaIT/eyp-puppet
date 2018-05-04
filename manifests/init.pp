7b5a83162f10a74843aa02681c2f61580822e589
Jordi Prats <jordi.prats@gmail.com>
2018-05-04 14:26:57 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,9 @@
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
 class puppet(
               $enable_puppetlabs_repo = $puppet::params::default_enable_puppetlabs_repo,
               $srcdir                 = '/usr/local/src',
               $ssldir                 = $puppet::params::ssldir_default,
               $basemodulepath         = '/etc/puppet/modules:/usr/share/puppet/modules',
             ) inherits puppet::params {
-
   if($enable_puppetlabs_repo)
   {
     class { 'puppet::puppetlabsrepo':
@@ -16,17 +11,16 @@ class puppet(
       srcdir                 => $srcdir,
     }
   }
-
   concat { $puppet::params::puppetconf:
-  	ensure => 'present',
-  	owner  => 'root',
-  	group  => 'root',
-  	mode   => '0644',
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
   concat::fragment{ 'puppetconf main':
-  	target  => '/etc/puppet/puppet.conf',
-  	order   => '00',
-  	content => template("${module_name}/puppetconf_main.erb"),
+    target  => '/etc/puppet/puppet.conf',
+    order   => '00',
+    content => template("${module_name}/puppetconf_main.erb"),
   }
 }
