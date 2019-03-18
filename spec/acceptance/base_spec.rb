8d0e5d0d78702dc55868a4241472abb97ea86d2b
Jordi Prats <jordi.prats@gmail.com>
2019-03-18 14:23:46 +0100

    only once due to tstenv not existing

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -47,7 +47,6 @@ describe 'puppet class' do
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
-      expect(apply_manifest(pp).exit_code).to eq(0)
     end
 
     describe file('/etc/puppetlabs/puppet/puppet.conf') do
