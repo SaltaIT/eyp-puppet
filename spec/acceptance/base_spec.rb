0ad0c01646ed10d1333150868b6420faf94038d8
Jordi Prats <jordi.prats@gmail.com>
2019-03-18 12:02:22 +0100

    acceptance environment

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -30,5 +30,37 @@ describe 'postgresql class' do
       its(:content) { should match 'masterport = 1234' }
     end
 
+    it "env production" do
+      expect(shell("puppet config print environment | grep production").exit_code).to be_zero
+    end
+
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'puppet::agent':
+        puppetmaster     => 'lolmaster',
+        puppetmasterport => '1234',
+        environment      => 'tst_env',
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe file('/etc/puppetlabs/puppet/puppet.conf') do
+      it { should be_file }
+      its(:content) { should match 'puppet managed file' }
+      its(:content) { should match 'server = lolmaster' }
+      its(:content) { should match 'masterport = 1234' }
+      its(:content) { should match 'environment=tst_env' }
+    end
+
+    it "env tst_env" do
+      expect(shell("puppet config print environment | grep tst_env").exit_code).to be_zero
+    end
+
   end
 end
